fd023d2fcbd12641cbf80903e6e6858077b9003d
Adem Rimpapa <72558944+a-rimpapa@users.noreply.github.com>
2020-10-10 23:56:09 +0200

    Added method body of file_to_tensor in utils.py

--- utils.py
+++ utils.py
@@ -1,2 +1,21 @@
+import torchvision.transforms as transforms
+import PIL
+
 def file_to_tensor(filename):
-    pass
+    # TODO docstring
+    img_raw = PIL.Image.open(filename)
+    width, height = img_raw.size
+    
+    # The original image must be square
+    assert (width == height), \
+    'The height and width of the input image must be equal.'
+    
+    preprocess_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
+    ])
+    
+    img = preprocess_transform(img_raw)
+    # shape of return value is (3, 224, 224)
+    return img
+
